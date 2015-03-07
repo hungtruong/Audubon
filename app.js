ff6d24d9ce74ee9a6a74a0542115bef837dd16e6
Hung Truong <hung@hung-truong.com>
2015-03-06 23:03:07 -0500

    code

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+
+ var spawn = require('child_process').spawn;
+
+ // Create a child process
+
+ console.log(req.query.url);
+
+
+ var child = spawn('phantomjs',
+     ['test3.js', req.query.url]);
+
+    child.stdout.on('data',
+    function (data) {
+        res.write(data);
+    });
+
+    child.on('exit', function (exitCode) {
+     res.end();
+    });
+
+  /*
+  var exec = require('child_process').exec,
+    child;
+
+  child = exec('phantomjs test3.js',
+  function (error, stdout, stderr) {
+    //console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    console.log('exec error: ' + error);
+    if (error == null) {
+      //console.log('exec error: ' + error);
+      console.log('got stuff');
+      res.type('png');
+      res.send(stdout);
+    }
+  });
+  //res.send('Hello World!')
+  */
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
---
+++ test3.js
@@ -0,0 +1,30 @@
+var page = require('webpage').create();
+
+var system = require('system');
+var args = system.args;
+
+var tweet_url = args[1];
+
+page.open(tweet_url, function    (status) {
+    if (status !== 'success') {
+        console.log('Unable to load the address!');
+    } else {
+        window.setTimeout(function () {
+            //Heres the actual difference from your code...
+            var bb = page.evaluate(function () {
+                return document.getElementsByClassName("permalink-tweet")[0].getBoundingClientRect();
+            });
+
+            page.clipRect = {
+                top:    bb.top,
+                left:   bb.left,
+                width:  bb.width,
+                height: bb.height
+            };
+
+	page.render("/dev/stdout", {format: 'png', quality: '100'});
+
+            phantom.exit();
+        }, 200);
+    }
+});
