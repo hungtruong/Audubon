f61c85209a811767268250c19971b34372248201
Hung Truong <hung@hung-truong.com>
2015-03-08 13:45:58 -0400

    clean up some js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 var spawn = require('child_process').spawn;
 
+//for static files, index.html etc
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/img', function (req, res) {
@@ -11,28 +12,18 @@ app.get('/img', function (req, res) {
  var id = parts[parts.length-1];
 
  var child = spawn('phantomjs',
-     ['test3.js', req.query.url]);
+     ['tweet_image.js', req.query.url]);
 
-    child.stdout.on('data',
-    function (data) {
-        //console.log('got some data');
-        //res.write(data);
-    });
-
-    child.on('exit', function (exitCode) {
-     //console.log('finished');
-     fs = require('fs')
-     data = fs.readFileSync('/tmp/' + id)
-     res.write(data);
-     res.end();
+     child.on('exit', function (exitCode) {
+      //when complete, grab the file from tmp and return it as a png
+      fs = require('fs')
+      data = fs.readFileSync('/tmp/' + id)
+      res.write(data);
+      res.end();
     });
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
-
   var host = server.address().address
   var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
 })
