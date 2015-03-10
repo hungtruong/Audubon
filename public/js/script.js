d6d7943bc0b2df8f7a05ccf7cc5a598a4fa776f7
Hung Truong <hung@hung-truong.com>
2015-03-09 22:18:25 -0400

    loading gif stuff

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,14 @@
 $( document ).ready(function() {
     $('form').submit(function(event){
+     $('#loading').show();
      var href = window.location.href;
      var tweet_url = $("#tweet-url-input").val();
      var img_url = href + "img/?url=" + tweet_url;
      $("#tweet-image").attr("src", img_url);
      event.preventDefault();
+    });
+
+    $("#tweet-image").on('load', function(){
+     $('#loading').hide();
     });
 });
