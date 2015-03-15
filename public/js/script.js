bc3150192264c605d33f9c148d96b02cbfe04338
Hung Truong <hung@hung-truong.com>
2015-03-15 14:45:43 -0400

    fix entering same url twice

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,13 @@ $( document ).ready(function() {
      var href = window.location.href;
      var tweet_url = $("#tweet-url-input").val();
      var img_url = href + "img/?url=" + tweet_url;
+     $("#tweet-image").remove();
+     $("#loading").after('<img id="tweet-image" src="">');
      $("#tweet-image").attr("src", img_url);
+     $("#tweet-image").on('load', function(){
+      $('#loading').hide();
+     });
      event.preventDefault();
     });
 
-    $("#tweet-image").on('load', function(){
-     $('#loading').hide();
-    });
 });
